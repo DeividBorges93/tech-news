9357b40d57c08fa99ce5ee50a1dd55b0d03b0926
DeividBorges93 <deividmborges@gmail.com>
2022-11-17 09:43:41 -0300

    feat(scraper): :sparkles: implementa as funções  scrape_noticia e get_tech_news
    scrape_noticia: recebe as info HTML de uma noticia, e preenche um dict com as innfo solicitada
    
    get_tech_news: recebe um número inteiro (n) como parametro e busca pelas últimas N noticias

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,8 @@ import requests
 import time
 from parsel import Selector
 
+from tech_news.database import create_news
+
 
 def fetch(url: str, wait: int = 3) -> str:
     try:
@@ -28,11 +30,73 @@ def scrape_next_page_link(html_content: str) -> list:
     return next_page
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    notice = dict()
+
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    notice["url"] = url
+
+    title = selector.css(".entry-title::text").get()
+
+    if title.endswith(" "):
+        title = title[:-1]
+
+    if title.endswith("\xa0\xa0\xa0"):
+        title = title[:-3]
+
+    notice["title"] = title
+
+    timestamp = selector.css(".meta-date::text").get()
+    notice["timestamp"] = timestamp
+
+    writer = selector.css(".url.fn.n::text").get()
+    notice["writer"] = writer
+
+    comments_count = selector.css(".comment-body").getall()
+    notice["comments_count"] = len(comments_count)
+
+    tags = selector.css("a[rel='tag']::text").getall()
+    notice["tags"] = tags
+
+    category = selector.css(".category-style span.label::text").get()
+    notice["category"] = category
+
+    summary = selector.css(
+        ".entry-content > p:nth-of-type(1) *::text"
+    ).getall()
+
+    summary = "".join(summary)
+
+    if summary.endswith(" ") or summary.endswith("\xa0"):
+        summary = summary[:-1]
+
+    notice["summary"] = summary
+
+    return notice
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    website_content = fetch("https://blog.betrybe.com")
+    next_page_link = ""
+    all_news_from_the_site = []
+
+    while next_page_link is not None:
+        if len(all_news_from_the_site) >= amount:
+            break
+
+        all_news_from_the_site.extend(scrape_novidades(website_content))
+        next_page_link = scrape_next_page_link(website_content)
+        website_content = fetch(next_page_link)
+
+    all_news_from_the_site = all_news_from_the_site[:amount]
+
+    notices = []
+    for notice in all_news_from_the_site:
+        website_content = fetch(notice)
+        notices.append(scrape_noticia(website_content))
+
+    create_news(notices)
+
+    return notices
