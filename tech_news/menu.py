a4bb5f37278dafb24ef479a3fee7ecaa094e013a
DeividBorges93 <deividmborges@gmail.com>
2022-11-17 10:00:53 -0300

    feat(menu): :sparkles: implementa chamada as funções de pessquisa e analise criadas anteriormente
    get_tech_news, search_by_title, search_by_date, search_by_tag, search_by_category,
    
    top_5_news, top_5_categories

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,45 @@
-# Requisito 12
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_tag,
+    search_by_title,
+)
+from tech_news.scraper import get_tech_news
+
+
+def analyzer_menu_get_tech_news():
+    amount = input("Digite quantas notícias serão buscadas:")
+    print(get_tech_news(int(amount)))
+
+
+def analyzer_menu_search_by_title():
+    title = input("Digite o título:")
+    print(search_by_title(title))
+
+
+def analyzer_menu_search_by_date():
+    title = input("Digite a data no formato aaaa-mm-dd:")
+    print(search_by_date(title))
+
+
+def analyzer_menu_search_by_tag():
+    title = input("Digite a tag:")
+    print(search_by_tag(title))
+
+
+def analyzer_menu_search_by_category():
+    title = input("Digite a categoria:")
+    print(search_by_category(title))
+
+
+def analyzer_menu_top_5_news():
+    print(top_5_news())
+
+
+def analyzer_menu_top_5_categories():
+    print(top_5_categories())
+
+
 def analyzer_menu():
     """Seu código deve vir aqui"""
