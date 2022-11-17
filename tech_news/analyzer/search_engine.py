cfd613e25057e8e98c070c1e221f3c6f084e1caf
DeividBorges93 <deividmborges@gmail.com>
2022-11-17 09:45:21 -0300

    feat(search): :sparkles: implementa funções de pesquisa por titulo, data tag e categoria
    funções case insensitive e populam uma lsita com as noticias

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,56 @@
-# Requisito 6
+from tech_news.database import find_news
+import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    notices = find_news()
+    response = []
+
+    for notice in notices:
+        if title.upper() in notice["title"].upper():
+            response.append((notice["title"], notice["url"]))
+
+    return response
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    notices = find_news()
+    response = []
+
+    try:
+        bool(datetime.datetime.strptime(date, "%Y-%m-%d"))
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    for notice in notices:
+        formatted_date = datetime.datetime.strptime(
+            notice["timestamp"], "%d/%m/%Y"
+        ).date()
+
+        if date == str(formatted_date):
+            response.append((notice["title"], notice["url"]))
+
+    return response
 
 
-# Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    notices = find_news()
+    response = []
+
+    for notice in notices:
+        for tag_notice in notice["tags"]:
+            if tag.upper() in tag_notice.upper():
+                response.append((notice["title"], notice["url"]))
+
+    return response
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    notices = find_news()
+    response = []
+
+    for notice in notices:
+        if category.upper() in notice["category"].upper():
+            response.append((notice["title"], notice["url"]))
+
+    return response
