868007cd4eec8b899cdc994b9fed57ff9096a012
DeividBorges93 <deividmborges@gmail.com>
2022-11-17 09:49:05 -0300

    feat(ratings): :sparkles: implementa funções que coletam as top 5 noticias e categorias
    top_5_news: busca as news pela get_collection e cria um lista dando um find() na coleção, etando os parametros
    
    top_5_categories: busca as news pela get_collection e cria uma lista usando a função aggregate

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,51 @@
-# Requisito 10
+from tech_news.database import get_collection
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_collection = get_collection()
+
+    five_news = list(
+        news_collection.find(
+            {},
+            {
+                "_id": False,
+                "title": True,
+                "url": True,
+                "comments_count": True,
+            },
+        )
+        .sort("comments_count", -1)
+        .limit(5)
+    )
+
+    five_formatted_news = []
+
+    for notice in five_news:
+        five_formatted_news.append((notice["title"], notice["url"]))
+
+    return five_formatted_news
 
 
-# Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_collection = get_collection()
+
+    ranking_categories = list(
+        news_collection.aggregate(
+            [
+                {
+                    "$group": {
+                        "_id": "$category",
+                        "count": {"$sum": 1},
+                    },
+                },
+                {"$sort": {"count": -1, "_id": 1}},
+            ]
+        )
+    )
+
+    categories = []
+
+    for notice in ranking_categories:
+        categories.append(notice["_id"])
+
+    return categories
